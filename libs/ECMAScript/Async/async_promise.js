5a2b61b8835429babc5f88ff4451091378ea1e82
huangsq <137707531@qq.com>
2025-08-15 01:31:40 +0800

    feat:  ac async_promise

---
+++ libs/ECMAScript/Async/async_promise.js
@@ -0,0 +1,224 @@
+(function () {
+  let allData = [];
+  /**
+   * @desc 1. 基础回调函数实现（原始模式）
+   * @returns {void}
+   */
+  function callbackHell() {
+    getAsyncData(1, (data1) => {
+      console.log('Callback 1:', data1);
+      allData.push(data1);
+      getAsyncData(2, (data2) => {
+        console.log('Callback 2:', data2);
+        allData.push(data2);
+        getAsyncData(3, (data3) => {
+          console.log('Callback 3:', data3);
+          allData.push(data3);
+          console.log('all data: ', allData);
+          allData = [];
+          // 更多嵌套...
+        });
+      });
+    });
+  }
+
+  /**
+   * @desc 2. Promise链式调用（ES6解决方案）
+   * @returns {Promise<void>}
+   */
+  function promiseChain() {
+    getPromise(1)
+      .then(data => {
+        console.log('Promise 1:', data);
+        allData.push(data);
+        return getPromise(2);
+      })
+      .then(data => {
+        console.log('Promise 2:', data);
+        allData.push(data);
+        return getPromise(3);
+      })
+      .then(data => {
+        console.log('Promise 3:', data);
+        allData.push(data);
+        console.log('all data: ', allData);
+        allData = [];
+      })
+      .catch(err => console.error('Promise Error:', err));
+  }
+
+  /**
+   * @desc 3. Generator+yield（过渡方案）
+   * @returns {Generator<any, any, any>}
+   */
+  function* generatorFlow() {
+    const data1 = yield getPromise(1);
+    console.log('Generator 1:', data1);
+    allData.push(data1);
+
+    const data2 = yield getPromise(2);
+    console.log('Generator 2:', data2);
+    allData.push(data2);
+
+    const data3 = yield getPromise(3);
+    console.log('Generator 3:', data3);
+    allData.push(data3);
+
+    const data4 = yield getPromise(4);
+    console.log('Generator 4:', data4);
+    allData.push(data4);
+
+    const data5 = yield getPromise(5);
+    console.log('Generator 5:', data5);
+    allData.push(data5);
+
+    const data6 = yield getPromise(6);
+    console.log('Generator 6:', data6);
+    allData.push(data6);
+
+    const data10 = yield getPromise(10);
+    console.log('Generator 10:', data10);
+    allData.push(data10);
+
+    const data11 = yield getPromise(11, 1000, 0);
+    console.log('Generator 11:', data11);
+    allData.push(data11);
+
+    const data12 = yield getPromise(12, 1000, 0);
+    console.log('Generator 12:', data12);
+    allData.push(data12);
+
+    const data13 = yield getPromise(13, 1000, 30);
+    console.log('Generator 13:', data13);
+    allData.push(data13);
+
+    const data14 = yield getPromise(14);
+    console.log('Generator 14:', data14);
+    allData.push(data14);
+
+    const data15 = yield getPromise(15, 1000, 40);
+    console.log('Generator 15:', data15);
+    allData.push(data15);
+
+    const data16 = yield getPromise(16, 1000, 50);
+    console.log('Generator 16:', data16);
+    allData.push(data16);
+
+    const data17 = yield getPromise(17, 1000, 60);
+    console.log('Generator 17:', data17);
+    allData.push(data17);
+
+    const data18 = yield getPromise(18, 1000, 70);
+    console.log('Generator 18:', data18);
+    allData.push(data18);
+
+    console.log('all data: ', allData);
+    allData = [];
+    return 'Generator Complete';
+  }
+
+  /**
+   * @desc 4. Async/Await（现代终极方案）Async/Await 顺序执行、使用try/catch捕获错误
+   * @returns {Promise<string>}
+   */
+  async function asyncAwaitFlow() {
+    try {
+      const data1 = await getPromise(1);
+      console.log('Async 1:', data1);
+      const data2 = await getPromise(2);
+      console.log('Async 2:', data2);
+      const data3 = await getPromise(3, 800, 50);
+      console.log('Async 3:', data3);
+      const data4 = await getPromise(4, 1200, 100);
+      console.log('Async 4:', data4);
+      const data5 = await getPromise(5);
+      console.log('Async 5:', data5);
+      const data6 = await getPromise(6);
+      console.log('Async 6:', data6);
+      return 'Async Complete';
+    } catch (err) {
+      console.error('Async Error:', err);
+    }
+  }
+
+  // ======== 工具函数 ========
+  // 模拟传统回调函数
+  function getAsyncData(id, callback) {
+    setTimeout(() => callback(`Data${id}`), 100);
+  }
+
+  /**
+   * @desc 封装为Promise
+   * @param {Number} id
+   * @param {Number} time
+   * @param {Number} error 失败的概率，0-100
+   * @returns {Promise}
+   */
+  function getPromise(id, time = 500, error = 0) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (error && Math.random() * 100 < error) {
+          reject(new Error(`Error${id}`));
+        } else {
+          const result = `Data${id}`;
+          console.log(`${result} is ready`);
+          resolve(result);
+        }
+      }, time);
+    });
+  }
+
+  /**
+   * @desc Generator执行器
+   * @param {() => Generator<any, any, any>} genFunc
+   */
+  function runGenerator(genFunc) {
+    const gen = genFunc();
+    /**
+     * @desc 递归执行生成器的下一步
+     * @param {() => IteratorResult<any, any>} nextFn
+     * @returns {Promise<any> | void}
+     */
+    function step(nextFn) {
+      /**
+       * @type {IteratorResult<any, any>}
+       */
+      let next;
+      try {
+        next = nextFn();
+      } catch (e) {
+        return console.error('Generator Error:', e);
+      }
+      if (next.done) return next.value;
+      Promise.resolve(next.value).then(
+        v => step(() => gen.next(v)),
+        e => step(() => gen.throw(e))
+      );
+    }
+    step(() => gen.next());
+  }
+
+  // ======== 执行控制 ========
+  const mode = process.argv[2] || 'async';
+  console.log(`\n===== Running ${mode.toUpperCase()} Mode =====`);
+
+  switch (mode) {
+    case 'callback':
+      callbackHell();
+      break;
+    case 'promise':
+      promiseChain();
+      break;
+    case 'generator':
+      runGenerator(generatorFlow);
+      break;
+    case 'async':
+      asyncAwaitFlow().then(res => console.log(res));
+      break;
+    default:
+      console.log('Invalid mode');
+  }
+})();
+
+// test
+// node Async/async_promise.js  async
--- libs/ECMAScript/Async/tests/async_promise.js
+++ libs/ECMAScript/Async/tests/async_promise.js
@@ -1,6 +1,27 @@
-(function (key) {
-
-  const getData = () => new Promise(resolve => setTimeout(() => resolve("data"), 1000));
+(function (k) {
+  console.log(process.argv);
+  const key = process.argv[2] ? parseInt(process.argv[2]) : k;
+  const PromiesTimeOut = (idx, time = 1000) => new Promise(resolve => setTimeout(() => resolve(`data${idx}`), time))
+  const PromiesTimeOut2 = (idx, time = 1000) => new Promise(resolve => resolve(`data${idx}`))
+  let getData = PromiesTimeOut, allData = [];
+  const chooicePromise = 1, time = 1000;
+  switch (chooicePromise) {
+    case 1:
+      getData = PromiesTimeOut;
+      break;
+    case 2:
+      getData = PromiesTimeOut2;
+      break;
+    case 3:
+      getData = PromiesTimeOut;
+      break;
+    case 4:
+      getData = PromiesTimeOut;
+      break;
+    default:
+      getData = PromiesTimeOut;
+      break;
+  }
   // 测试 promise 顺序传递结果给调用方
   function testPromise() {
     async function async1() {
@@ -25,81 +46,153 @@
     });
   }
 
+  function testTryCatch() {
+    try {
+      new Promise((rs, rj) => {
+        console.log('Promise:start');
+        // rs(new Error('manual error'));
+        throw new Error('error');
+      }).then(res => {
+        console.log('Promise:then', res);
+      }).catch(err => {
+        console.log('Promise:catch', err);
+      }).finally(() => {
+        console.log('finally');
+      });
+    } catch (error) {
+      // 无法捕获到错误、因为promise的错误不会被catch捕获到
+      console.log('out:catch', error);
+    }
+  }
+
+  /**
+   * @desc promise 的并发执行
+   */
+  function testPromiseConcurrent() {
+    (() => {
+      const data = getData(1);
+      console.log('data: ', data);
+      const data2 = getData(2);
+      console.log('data2: ', data2);
+      const data3 = getData(3);
+      console.log('data3: ', data3);
+      const data4 = getData(4);
+      console.log('data4: ', data4);
+      const data5 = getData(5);
+      console.log('data5: ', data5);
+      const allData = [data, data2, data3, data4, data5];
+      console.log('all data: ', allData);
+    })()
+  }
+
+
+  /**
+   * @desc promise 的顺序执行、使用then的嵌套、不使用async/await
+   */
+  function testPromiseChain1() {
+    getData(1).then(res => {
+      console.log('data: ', res);
+      allData.push(res);
+      getData(2).then(res => {
+        allData.push(res);
+        console.log('data: ', res);
+        getData(3).then(res => {
+          allData.push(res);
+          console.log('data: ', res);
+          getData(4).then(res => {
+            allData.push(res);
+            console.log('data: ', res);
+            getData(5).then(res => {
+              allData.push(res);
+              console.log('data: ', res);
+              console.log('all data: ', allData);
+            });
+          });
+        });
+      });
+    });
+  }
+
+
+  /**
+   * 使用generator 实现promise的顺序执行
+   * @returns
+   */
+  function* testPromiseGenerator() {
+    const data1 = yield getData(1);
+    console.log('data1: ', data1);
+    allData.push(data1);
+    const data2 = yield getData(2);
+    console.log('data2: ', data2);
+    allData.push(data2);
+    const data3 = yield getData(3);
+    console.log('data3: ', data3);
+    allData.push(data3);
+    const data4 = yield getData(4);
+    console.log('data4: ', data4);
+    allData.push(data4);
+    const data5 = yield getData(5);
+    console.log('data5: ', data5);
+    allData.push(data5);
+    const data6 = yield getData(6);
+    console.log('data6: ', data6);
+    allData.push(data6);
+    console.log('all data: ', allData);
+    allData = [];
+    return 'success';
+  }
+
+  /**
+   * @desc promise 的顺序执行、使用async/await
+   * @returns
+   */
+  async function testPromiseAsyncAwait1() {
+    const data = await getData(1);
+    console.log('data: ', data);
+    const data2 = await getData(2);
+    console.log('data2: ', data2);
+    const data3 = await getData(3);
+    console.log('data3: ', data3);
+    const data4 = await getData(4);
+    console.log('data4: ', data4);
+    const data5 = await getData(5);
+    console.log('data5: ', data5);
+    const data6 = await getData(6);
+    console.log('data6: ', data6);
+    allData = [data, data2, data3, data4, data5, data6];
+    console.log('all data: ', allData);
+    allData = [];
+    return 'success';
+  }
   switch (key) {
-    case 1:
+    case -1:
       testPromise();
       break;
+    case 0:
+      testTryCatch();
+      break;
+    case 1:
+      testPromiseChain1();
+      break;
     case 2:
-
-      try {
-        new Promise((rs, rj) => {
-          console.log('Promise:start');
-          // rs(new Error('manual error'));
-          throw new Error('error');
-        }).then(res => {
-          console.log('Promise:then', res);
-        }).catch(err => {
-          console.log('Promise:catch', err);
-        }).finally(() => {
-          console.log('finally');
-        });
-      } catch (error) {
-        console.log('out:catch', error);
-      }
+      testPromiseConcurrent();
       break;
     case 3:
-      async function test() {
-        const data1 = await getData();
-        console.log(`data1: ${data1}`);
-        const data2 = await getData();
-        console.log(`data2: ${data2}`);
-        const data3 = await getData();
-        console.log(`data3: ${data3}`);
-        const data4 = await getData();
-        console.log(`data4: ${data4}`);
-        const data5 = await getData();
-        console.log(`data5: ${data5}`);
-        const data6 = await getData();
-        console.log(`data6: ${data6}`);
-        const data7 = await getData();
-        console.log(`data7: ${data7}`);
-        const data8 = await getData();
-        console.log(`data8: ${data8}`);
-        const data9 = await getData();
-        return 'success';
-      }
-      // 这样的一个函数 应该再1秒后打印data 再过一秒打印data2 最后打印success
-      test().then(res => console.log(res));
+      const gen = testPromiseGenerator();
+      gen.next();
+      gen.next();
+      gen.next();
+      gen.next();
+      gen.next();
       break;
     case 4:
-      var test = asyncToGenerator(
-        function* testG() {
-          // await被编译成了yield
-          const data = yield getData();
-          console.log('data: ', data);
-          const data2 = yield getData();
-          console.log('data2: ', data2);
-          return 'success';
-        }
-      );
-
-      testG().then(res => console.log(res));
+      break;
+    case 5:
+      testPromiseAsyncAwait1();
+      break;
+    case 6:
       break;
     default:
       break;
   }
-})(4);
-
-// 错误答案
-// script start script end  async1 start promise1 async2 promise2 async1 end setTimeout
-
-// 正确答案
-//
-// script start
-// async1 start
-// async2
-// promise1
-// script end
-// async1 end
-// promise2
-// setTimeout
+})();
